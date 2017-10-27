cceaad978d88b35a98f5c868fdc3d7326d2cc26d
Thiago Loureiro <thiagoguaru@me.com>
2017-10-27 20:15:33 -0200

    DBProject

---
+++ DBProject/dbo/Tables/Groups.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Groups] (
+    [Id]         UNIQUEIDENTIFIER NOT NULL,
+    [Name]       NVARCHAR (MAX)   NULL,
+    [IsPublic]   BIT              NOT NULL,
+    [AvatarUrl]  NVARCHAR (MAX)   NULL,
+    [TypeId]     UNIQUEIDENTIFIER NOT NULL,
+    [CreatedOn]  DATETIME         NOT NULL,
+    [UpdatedOn]  DATETIME         NULL,
+    [ArchivedOn] DATETIME         NULL,
+    CONSTRAINT [PK_dbo.Groups] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_TypeId]
+    ON [dbo].[Groups]([TypeId] ASC);
+
---
+++ DBProject/dbo/Tables/Messages.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Messages] (
+    [Id]            UNIQUEIDENTIFIER NOT NULL,
+    [GroupId]       UNIQUEIDENTIFIER NOT NULL,
+    [ParentId]      UNIQUEIDENTIFIER NULL,
+    [CreatedById]   UNIQUEIDENTIFIER NOT NULL,
+    [TypeId]        UNIQUEIDENTIFIER NOT NULL,
+    [Text]          NVARCHAR (MAX)   NULL,
+    [AttachmentUrl] NVARCHAR (MAX)   NULL,
+    [CreatedOn]     DATETIME         NOT NULL,
+    [UpdatedOn]     DATETIME         NULL
+);
+
+
